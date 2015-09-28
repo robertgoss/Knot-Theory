700b3f358b1d85cebc294f5df8aff849bac72832
Robert <goss.robert@gmail.com>
2015-09-27 19:29:50 -0700

    Outlined the consistency conditions required for valid link diagram data.

--- src/LinkDiagram/Internal.hs
+++ src/LinkDiagram/Internal.hs
@@ -55,8 +55,30 @@ data LinkDiagramData = LinkDiagramData {
 
 
 --Returns if the given link diagram data is consistent and constructs a valid link
---In particular it checks that:
--- 
+--In particular it checks the following relations:
+-- For each crossing:
+--  - Each edge mentioned in the crossing meets the crossing once and each loop twice.
+--  - The first edge in the crossing is incomming to the crossing.
+--  - The local regions on either side of neighbouring edges are the same.
+-- For each edge:
+--  - Each crossing the edge meets contains the edge.
+--  - Each region the edge meets has that edge in one of its boundaries.
+--  - The 2 regions which meet the edge are distinct.
+--  - The component of the region is a path containing the edge.
+-- For each unknot
+--  - Each region the unknot meets has that unknot in one of its boundaries.
+--  - The 2 regions which meet the unknot are distinct.
+--  - One of the regions is bounded only be this unknot.
+--  - Their exists a unique component which is this unknot.
+-- For each region
+--  - Each unknot in the boundary of the region meets this unknot.
+--  - Each edge in a boundary of the region meets the region.
+--  - No two edges in different boundaries of the region can be equal.
+-- For each component
+--  - If this is a path component then for each edge in the path has this as a component
+--  - If this is a path component then for any sequential pair of edges in the path
+--       the end crossing of the first and start crossing of the second are the same
+--       and the edges meet at the crossing as opposite.
 --These confirm it is a valid link 
 -- We further require and required consistency conditions are met for looking up indices
 -- Which are that and vertex, edges etc index mentioned in a vertex, edge, etc is in the respective table.
