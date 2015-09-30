c0aae41ae22e7abfd3888031eef9aa44909f2481
Robert Goss <goss.robert@gmail.com>
2015-09-30 20:53:09 +0100

    Added a test for edgeIndices and added type hints for hlint.

--- tests/Test/LinkDiagram/CrossingTest.hs
+++ tests/Test/LinkDiagram/CrossingTest.hs
@@ -11,9 +11,19 @@ import Test.Tasty.HUnit
 
 --Test Crossing module
 testCrossing :: TestTree
-testCrossing = testGroup "Test Crossing" [ testFromEdges,
+testCrossing = testGroup "Test Crossing" [ testEdgeIndicies,
+                                           testFromEdges,
                                            testMeetingNumber,
                                            testOpposite]
+
+--Test edgeIndices
+testEdgeIndicies :: TestTree
+testEdgeIndicies = testGroup "Test edgeIndices" [testEdgeIndices4Element] 
+
+testEdgeIndices4Element :: TestTree
+testEdgeIndices4Element = SC.testProperty "edgeIndices should always produce a 4 element list." edgeIndices4ElementTest
+    where edgeIndices4ElementTest :: TestingCrossing Int -> Bool
+          edgeIndices4ElementTest = (==4) . length . edgeIndices . tCrossing
 
 --Test fromEdges
 testFromEdges :: TestTree
@@ -29,31 +39,42 @@ testFromEdges = testGroup "Test fromEdges" [ testFromEdgesInverse,
                                            ]
 
 --Prop
+
 --Edgeindex and fromEdges mutual inverse
 -- With just a wrapper and we convert the output of edgeIndices to edgeIndices.
+testFromEdgesInverse :: TestTree
 testFromEdgesInverse = SC.testProperty "fromEdges is an inverse to edgeIndices" fromEdgesInverseTest
       where fromEdgesInverseTest :: TestingCrossing Int -> Bool
             fromEdgesInverseTest tCross = Just crossing == (fromEdges . toTuple . edgeIndices) crossing
                 where crossing = tCrossing tCross
                       toTuple [e1,e2,e3,e4] = (e1,e2,e3,e4)
+                      toTuple _ = error "edgeIndices should always return 4 element list"
 
 --Test various local constraint are invalid:
-testFromEdges3EdgesInvalid = testCase "A crossing with 3 edges is invalid" $ Nothing @?= fromEdges (1,2,1,1)
+testFromEdges3EdgesInvalid :: TestTree
+testFromEdges3EdgesInvalid = testCase "A crossing with 3 edges is invalid" $ Nothing @?= fromEdges (1::Int,2,1,1)
 
-testFromEdges4EdgesInvalid = testCase "A crossing with 4 edges is invalid" $ Nothing @?= fromEdges (2,2,2,2)
+testFromEdges4EdgesInvalid :: TestTree
+testFromEdges4EdgesInvalid = testCase "A crossing with 4 edges is invalid" $ Nothing @?= fromEdges (2::Int,2,2,2)
 
-testFromEdgesVOppositeInvalid = testCase "A crossing with a vertical loop is invalid" $ Nothing @?= fromEdges (1,2,1,3)
+testFromEdgesVOppositeInvalid :: TestTree
+testFromEdgesVOppositeInvalid = testCase "A crossing with a vertical loop is invalid" $ Nothing @?= fromEdges (1::Int,2,1,3)
 
-testFromEdgesHOppositeInvalid = testCase "A crossing with a horizontal loop is invalid" $ Nothing @?= fromEdges (1,2,3,2)
+testFromEdgesHOppositeInvalid :: TestTree
+testFromEdgesHOppositeInvalid = testCase "A crossing with a horizontal loop is invalid" $ Nothing @?= fromEdges (1::Int,2,3,2)
 
-testFromEdgesDoubleLoopOppositeInvalid = testCase "A double loop crossing with a horizontal loop is invalid" $ Nothing @?= fromEdges (1,2,1,2)
+testFromEdgesDoubleLoopOppositeInvalid :: TestTree
+testFromEdgesDoubleLoopOppositeInvalid = testCase "A double loop crossing with a horizontal loop is invalid" $ Nothing @?= fromEdges (1::Int,2,1,2)
 
 --Some unit tests to make sure a crossing loop and double loop are correctly created.
-testCrossingCreated = testCase "Can correctly construct valid crossing" $ Just (Crossing 1 2 3 4) @?= fromEdges (1,2,3,4)
+testCrossingCreated :: TestTree
+testCrossingCreated = testCase "Can correctly construct valid crossing" $ Just (Crossing 1 2 3 4) @?= fromEdges (1::Int,2,3,4)
 
-testLoopCreated = testCase "Can correctly construct valid crossing with a loop" $ Just (LoopCrossingTR 3 1 2) @?= fromEdges (1,2,3,3)
+testLoopCreated :: TestTree
+testLoopCreated = testCase "Can correctly construct valid crossing with a loop" $ Just (LoopCrossingTR 3 1 2) @?= fromEdges (1::Int,2,3,3)
 
-testDoubleLoopCreated = testCase "Can correctly construct valid crossing with 2 loops" $ Just (DoubleLoopL  1 2) @?= fromEdges (1,1,2,2)
+testDoubleLoopCreated :: TestTree
+testDoubleLoopCreated = testCase "Can correctly construct valid crossing with 2 loops" $ Just (DoubleLoopL  1 2) @?= fromEdges (1::Int,1,2,2)
 
 --Test meeting number
 testMeetingNumber :: TestTree
@@ -68,26 +89,33 @@ testMeetingNumber = testGroup "Test edgeMeetingNumber" [testMeetingNumberNonEdge
 
 --Test the various combinations of a crossing type and if an edge
 -- either doesn't meet the crossing, meets the crossing in one place or as a loop.
+testMeetingNumberNonEdgeWithCrossing :: TestTree
 testMeetingNumberNonEdgeWithCrossing = testCase "Meeting of non edge with a crossing"
-                                            $ 0 @?= edgeMeetingNumber (Crossing 1 3 5 7) 2
+                                            $ 0 @?= edgeMeetingNumber (Crossing (1::Int) 3 5 7) 2
 
+testMeetingNumberNonEdgeWithLoop :: TestTree
 testMeetingNumberNonEdgeWithLoop = testCase "Meeting of non edge with a looped crossing"
-                                            $ 0 @?= edgeMeetingNumber (Crossing 1 1 5 7) 2
+                                            $ 0 @?= edgeMeetingNumber (Crossing (1::Int) 1 5 7) 2
 
+testMeetingNumberNonEdgeWithDLoop :: TestTree
 testMeetingNumberNonEdgeWithDLoop = testCase "Meeting of non edge with a double looped crossing"
-                                            $ 0 @?= edgeMeetingNumber (Crossing 1 1 7 7) 2
+                                            $ 0 @?= edgeMeetingNumber (Crossing (1::Int) 1 7 7) 2
 
+testMeetingNumberEdgeWithCrossing :: TestTree
 testMeetingNumberEdgeWithCrossing = testCase "Meeting of edge with a crossing"
-                                            $ 1 @?= edgeMeetingNumber (Crossing 1 3 5 7) 1
+                                            $ 1 @?= edgeMeetingNumber (Crossing (1::Int) 3 5 7) 1
 
+testMeetingNumberEdgeWithLoop :: TestTree
 testMeetingNumberEdgeWithLoop = testCase "Meeting of edge with a looped crossing"
-                                            $ 1 @?= edgeMeetingNumber (Crossing 1 1 5 7) 5
+                                            $ 1 @?= edgeMeetingNumber (Crossing (1::Int) 1 5 7) 5
 
+testMeetingNumberLoopWithLoop :: TestTree
 testMeetingNumberLoopWithLoop = testCase "Meeting of loop with a looped crossing"
-                                            $ 2 @?= edgeMeetingNumber (Crossing 1 1 5 7) 1
+                                            $ 2 @?= edgeMeetingNumber (Crossing (1::Int) 1 5 7) 1
 
+testMeetingNumberLoopWithDLoop :: TestTree
 testMeetingNumberLoopWithDLoop = testCase "Meeting of loop with a double looped crossing"
-                                            $ 2 @?= edgeMeetingNumber (Crossing 1 1 7 7) 7
+                                            $ 2 @?= edgeMeetingNumber (Crossing (1::Int) 1 7 7) 7
 
 --Test oppositeEdges
 testOpposite :: TestTree
@@ -100,22 +128,28 @@ testOpposite = testGroup "Test oppositeEdges" [testOppositeCorrectCrossing,
                                               ]
 
 --Test various crossing types recognise opposite edges
---We add a Int type specifier to alow com
+--We add a Int type specifier to allow com
+testOppositeCorrectCrossing :: TestTree
 testOppositeCorrectCrossing = testCase "Opposite edges on a crossing"
-                                           $ True @?= oppositeEdges (Crossing 1 3 5 7) 1 5
+                                           $ True @?= oppositeEdges (Crossing (1::Int) 3 5 7) 1 5
 
+testOppositeCorrectLoop :: TestTree
 testOppositeCorrectLoop = testCase "Opposite edges on a looped crossing"
-                                           $ True @?= oppositeEdges  (Crossing 1 1 5 7) 1 7
+                                           $ True @?= oppositeEdges  (Crossing (1::Int) 1 5 7) 1 7
 
+testOppositeCorrectDLoop :: TestTree
 testOppositeCorrectDLoop = testCase "Opposite edges on a double looped crossing"
-                                           $ True @?= oppositeEdges  (Crossing 1 1 2 2) 1 2
+                                           $ True @?= oppositeEdges  (Crossing (1::Int) 1 2 2) 1 2
 
 --Test various crossing types recognise edges which are not opposite
+testOppositeIncorrectCrossing :: TestTree
 testOppositeIncorrectCrossing = testCase "Non-opposite edges on a crossing"
-                                           $ False @?= oppositeEdges  (Crossing 1 3 5 7) 1 3
+                                           $ False @?= oppositeEdges  (Crossing (1::Int) 3 5 7) 1 3
 
+testOppositeIncorrectLoop :: TestTree
 testOppositeIncorrectLoop = testCase "Non-opposite edges on a looped crossing"
-                                           $ False @?= oppositeEdges  (Crossing 1 1 5 7) 1 1
+                                           $ False @?= oppositeEdges  (Crossing (1::Int) 1 5 7) 1 1
 
+testOppositeIncorrectDLoop :: TestTree
 testOppositeIncorrectDLoop = testCase "Non-opposite edges on a double looped crossing"
-                                           $ False @?= oppositeEdges  (Crossing 1 1 2 2) 2 2
+                                           $ False @?= oppositeEdges  (Crossing (1::Int) 1 2 2) 2 2
